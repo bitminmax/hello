a37e4f36fc6ebe478742d035a26b11b77b696afd
guolong <1735833868@qq.com>
2022-07-02 16:59:49 +0800

    new file hello

---
+++ springboot-02/src/main/java/com/example/springboot02/pojo/Null.java
@@ -0,0 +1,5 @@
+package com.example.springboot02.pojo;
+
+public class Null {
+    private  String st;
+}
---
+++ springboot-02/src/main/java/com/example/springboot02/pojo/Test.java
@@ -0,0 +1,7 @@
+package com.example.springboot02.pojo;
+
+public class Test {
+    void s(){
+        System.out.println("你好");
+    }
+}
